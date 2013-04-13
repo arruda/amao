d5b8efa22626f54a7786e5148b95a5a251ab44a1
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-04-13 00:29:26 -0300

    criando view de edicao refs #92

--- AMAO/apps/Avaliacao/Questao/views/criar.py
+++ AMAO/apps/Avaliacao/Questao/views/criar.py
@@ -10,17 +10,18 @@ from Professor.models import Professor, Monitor
 from Avaliacao.Questao.models import Questao ,EntradaGabarito, FonteGabarito ,OpcaoMultiplaEscolha
 from Avaliacao.Questao.forms import criarQuestaoForm,criarTipoQuestaoForm
 
+from AMAO.libs.forms_amao.widgets import change_widget_to_NoFullPathLinkFileInput
 @prof_monit_exist
-@login_required  
+@login_required
 @render_to('avaliacao/questao/criar.html')
-def criar_questao(request):    
-    autor = request.user 
+def criar_questao(request):
+    autor = request.user
     criado=False
     #QuestoesFormsSet = formset_factory(criarFiltroQuestaoForm)
     formsetEntradasInline=inlineformset_factory(Questao,EntradaGabarito,extra=1)
     formsetFontesInline=inlineformset_factory(Questao,FonteGabarito,extra=1)
     formsetOpcoesInline=inlineformset_factory(Questao,OpcaoMultiplaEscolha,extra=4)
-    if request.method == "POST":        
+    if request.method == "POST":
         form = criarQuestaoForm(request.POST)
         if form.is_valid():
             novaQuestao=form.save(commit=False)
@@ -28,15 +29,15 @@ def criar_questao(request):
             formEntradas = formsetEntradasInline(request.POST, request.FILES,instance=novaQuestao)
             formFontes = formsetFontesInline(request.POST, request.FILES,instance=novaQuestao)
             formOpcoes = formsetOpcoesInline(request.POST,instance=novaQuestao)
-                
+
             if formEntradas.is_valid() and formFontes.is_valid() and formOpcoes.is_valid():
                 novaQuestao.save()
-                formEntradas.save()  
-                formFontes.save() 
-                formOpcoes.save()    
+                formEntradas.save()
+                formFontes.save()
+                formOpcoes.save()
                 form.save_m2m()
                 novaQuestao.save(verificar=True)
-                criado=True  
+                criado=True
         else:
             #QuestoesForms=formsetInline()
             formEntradas = formsetEntradasInline()
@@ -48,19 +49,63 @@ def criar_questao(request):
         formEntradas = formsetEntradasInline()
         formFontes = formsetFontesInline()
         formOpcoes = formsetOpcoesInline()
-        
+
     return locals()
 
-@login_required  
+@prof_monit_exist
+@login_required
+@render_to('avaliacao/questao/editar.html')
+def editar_questao(request,questao_id):
+
+
+    questao = get_object_or_404(Questao,pk=questao_id)
+    autor = request.user
+    criado=False
+    formsetEntradasInline=inlineformset_factory(Questao,EntradaGabarito,formfield_callback=change_widget_to_NoFullPathLinkFileInput,extra=1)
+    formsetFontesInline=inlineformset_factory(Questao,FonteGabarito,formfield_callback=change_widget_to_NoFullPathLinkFileInput,extra=1)
+    formsetOpcoesInline=inlineformset_factory(Questao,OpcaoMultiplaEscolha,extra=4)
+    if request.method == "POST":
+        form = criarQuestaoForm(request.POST,instance=questao)
+        if form.is_valid():
+            questao=form.save(commit=False)
+            questao.autor = autor
+            formOpcoes = formsetOpcoesInline(request.POST,instance=questao)
+            formEntradas = formsetEntradasInline(request.POST, request.FILES,instance=questao)
+            formFontes = formsetFontesInline(request.POST, request.FILES,instance=questao)
+
+            if formEntradas.is_valid() and formOpcoes.is_valid() and formFontes.is_valid():
+                questao.save()
+                formEntradas.save()
+                formFontes.save()
+                formOpcoes.save()
+                form.as_p()
+                form.save_m2m()
+                questao.save(verificar=True)
+                criado=True
+        # else:
+        #     #QuestoesForms=formsetInline()
+        #     formEntradas = formsetEntradasInline(instance=questao)
+        #     formFontes = formsetFontesInline(instance=questao)
+        #     formOpcoes = formsetOpcoesInline(instance=questao)
+    else:
+        #QuestoesForms=formsetInline()
+        form = criarQuestaoForm(instance=questao)
+        formOpcoes = formsetOpcoesInline(instance=questao)
+        formEntradas = formsetEntradasInline(instance=questao)
+        formFontes = formsetFontesInline(instance=questao)
+
+    return locals()
+
+@login_required
 @render_to('avaliacao/questao/criar_tipo.html')
-def criar_tipo(request):     
-    criado=False    
-    if request.method == "POST":        
+def criar_tipo(request):
+    criado=False
+    if request.method == "POST":
         form = criarTipoQuestaoForm(request.POST)
-        if form.is_valid():           
+        if form.is_valid():
             form.save()
-            criado=True  
+            criado=True
     else:
         form = criarTipoQuestaoForm()
-        
+
     return locals()
